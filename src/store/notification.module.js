05203ed7e42b8441e1e935311c2decd0130044e1
Vincent-W0ng <1808803474@qq.com>
2025-06-25 17:31:26 +0800

    1

---
+++ src/store/notification.module.js
@@ -0,0 +1,31 @@
+// store/notification.module.js
+const state = {
+  currentNotification: null
+}
+
+const mutations = {
+  SET_NOTIFICATION(state, notification) {
+    state.currentNotification = notification
+  },
+  CLEAR_NOTIFICATION(state) {
+    state.currentNotification = null
+  }
+}
+
+const actions = {
+  showNotification({ commit }, notification) {
+    commit('SET_NOTIFICATION', notification)
+    if (notification.timeout) {
+      setTimeout(() => {
+        commit('CLEAR_NOTIFICATION')
+      }, notification.timeout)
+    }
+  }
+}
+
+export default {
+  namespaced: true, // 必须启用命名空间
+  state,
+  mutations,
+  actions
+}
